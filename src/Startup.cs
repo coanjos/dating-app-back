fd541ce2b13683040d7a92a1bbb86bb9f72c5e4c
Caio dos Anjos <coa.anjos@gmail.com>
2020-06-23 08:56:57 -0300

    Add cors

--- src/Startup.cs
+++ src/Startup.cs
@@ -37,6 +37,8 @@ namespace DatingApp
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +62,8 @@ namespace DatingApp
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
